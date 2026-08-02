090e35e7cdbef8168b4136108c8fa86c9c0da922
Ashik <ashikmdt86@gmail.com>
2026-08-02 15:20:13 +0530

    Implement React routing and navbar navigation

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom"
+
+
 function Navbar() {
   return (
     <nav>
-      Navbar
+
+      <Link to="/">
+        Home
+      </Link>
+
+      <Link to="/about">
+        About
+      </Link>
+
+      <Link to="/projects">
+        Projects
+      </Link>
+
+      <Link to="/contact">
+        Contact
+      </Link>
+
     </nav>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDom from "react-dom/client"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from "./App"
+import './index.css'
+import App from './App.jsx'
 
-
-ReactDom.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 )
